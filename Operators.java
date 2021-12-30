98c0c3ec309612117f29a6fcab7fa04cce123e2e
PoojaMangulkar <96859422+PoojaMangulkar@users.noreply.github.com>
2021-12-30 13:46:09 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,48 @@
+
+public class Operators {
+	 public static void main(String[] args) {
+		  
+		    int a = 12, b = 5;
+
+		 
+		    System.out.println("a + b = " + (a + b));
+
+		
+		    System.out.println("a - b = " + (a - b));
+
+		    System.out.println("a * b = " + (a * b));
+
+	
+		    System.out.println("a / b = " + (a / b));
+
+		    System.out.println("a % b = " + (a % b));
+		    
+		    int A = 4;
+		    int var;
+
+		    var = A;
+		    System.out.println("var using =: " + var);
+
+		 
+		    var += A;
+		    System.out.println("var using +=: " + var);
+
+		    var *= A;
+		    System.out.println("var using *=: " + var);
+		    
+		  
+		    System.out.println((5 > 3) && (8 > 5)); 
+		    System.out.println((5 > 3) && (8 < 5));  
+
+		
+		    System.out.println((5 < 3) || (8 > 5));  
+		    System.out.println((5 > 3) || (8 < 5));  
+		    System.out.println((5 < 3) || (8 < 5)); 
+
+		  
+		    System.out.println(!(5 == 3));  
+		    System.out.println(!(5 > 3)); 
+		  }
+		
+
+}
