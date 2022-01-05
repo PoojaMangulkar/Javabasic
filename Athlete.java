3758d277b46603bce321a13dc79cd599da7f0193
PoojaMangulkar <96859422+PoojaMangulkar@users.noreply.github.com>
2022-01-05 13:40:41 +0530

    Add files via upload

--- Athlete.java
+++ Athlete.java
@@ -1,43 +1,27 @@
-package Polymorphism;
+package Inheritance;
 
-public abstract class Athlete {
-	private String name;
-    private String nickname;
-    private int yearOfBorn;
-    private String team;
-    private int numberOfCompetitions;
+public class Athlete {
+	 private String name;
+	    private String nickname;
+	    private int yearOfBorn;
+	    private String team;
+	    private int gamesPlayed;
 
-    public Athlete(String name, String nickname, int yearOfBorn, String team, int numberOfCompetitions) {
-        this.name = name;
-        this.nickname = nickname;
-        this.yearOfBorn = yearOfBorn;
-        this.team = team;
-        this.numberOfCompetitions = numberOfCompetitions;
-    }
+	    public Athlete(String name, String nickname, int yearOfBorn, String team, int gamesPlayed) {
+	        this.name = name;
+	        this.nickname = nickname;
+	        this.yearOfBorn = yearOfBorn;
+	        this.team = team;
+	        this.gamesPlayed = gamesPlayed;
+	    }
 
-    public String getName() {
-        return name;
-    }
+	    public String getName() {
+	        return name;
+	    }
 
-    public void getBio() {
-        System.out.println(name + " (" + nickname + ")");
-        System.out.println("Born in " + yearOfBorn);
-        System.out.println("Last team is " + team + " and took part in " + numberOfCompetitions + " competitions");
-    }
-
-    public void compete() {
-        System.out.println(name + " took part in a competition.");
-        numberOfCompetitions++;
-    }
-
-    public String getTeam() {
-        return team;
-    }
-
-    public void setTeam(String team) {
-        this.team = team;
-    }
-
-    public abstract String getBodyType();
-
+	    public void getBio() {
+	        System.out.println(name + " (" + nickname + ")");
+	        System.out.println("Born in " + yearOfBorn);
+	        System.out.println("Last team is " + team + " and played " + gamesPlayed + " games");
+	    }
 }
